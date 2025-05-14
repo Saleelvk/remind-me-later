bec9d87a27dfad43122f33c64513d8f2981fbe81
Saleel vk <87797952+Saleelvk@users.noreply.github.com>
2025-05-14 11:37:09 +0530

    Update reminderRoutes.js

--- backend/routes/reminderRoutes.js
+++ backend/routes/reminderRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createReminder } = require('../controllers/reminderController');
 
-// POST endpoint to create a reminder
+
 router.post('/create', createReminder);
 
 module.exports = router;
